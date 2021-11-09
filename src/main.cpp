2b6a5447daa73defa73858c4722d9a0262c496b0
gofuwako <gofuwako@gmail.com>
2021-11-09 12:53:54 +0100

    Add and fix clash init

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,9 @@ int main(int argc, char *argv[])
     about.processCommandLine(&parser);
     const QStringList args = parser.positionalArguments();
 
+    // Init clash core
+    initClashCore();
+
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
 
